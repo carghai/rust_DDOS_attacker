c763f8f4c31fa441449da14b4aade4e44dc4b6a1
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-10-26 14:49:42 -0400

    code

--- src/extra_fn.rs
+++ src/extra_fn.rs
@@ -1,6 +1,6 @@
-use std::{thread, time};
 use std::net::UdpSocket;
 use std::sync::MutexGuard;
+use std::{thread, time};
 
 use reqwest::{Error, RequestBuilder, Response};
 
@@ -18,22 +18,19 @@ pub(crate) fn time_function() {
     }
 }
 
-
 pub(crate) fn proxy_set(url: &str, proxy: bool) -> Result<String, Error> {
     if proxy {
         let proxy_set = reqwest::Proxy::all(url);
         match proxy_set {
             Err(e) => Err(e),
             Ok(good) => {
-                let final_check = reqwest::Client::builder()
-                    .proxy(good)
-                    .build();
+                let final_check = reqwest::Client::builder().proxy(good).build();
                 match final_check {
                     Err(e) => Err(e),
                     Ok(final_data) => unsafe {
                         UNSAFE_PUB_VAR.http_sender = request_builder(final_data);
                         Ok("Proxy has been set!".to_owned())
-                    }
+                    },
                 }
             }
         }
@@ -47,36 +44,44 @@ pub(crate) fn proxy_set(url: &str, proxy: bool) -> Result<String, Error> {
 
 pub(crate) async fn request() -> Result<Response, Error> {
     unsafe {
-        let request = UNSAFE_PUB_VAR.http_sender.try_clone();
-        match request {
-            Some(sender) => sender.send().await,
-            None => {
-                println!("Ram Error, Lower Threads");
-                reqwest::Client::new().get(&UNSAFE_PUB_VAR.attack_url).send().await
+        if !UNSAFE_PUB_VAR.attack_mode {
+            let request = UNSAFE_PUB_VAR.http_sender.try_clone();
+            match request {
+                Some(sender) => sender.send().await,
+                None => {
+                    println!("Ram Error, Lower Threads");
+                    request_builder(reqwest::Client::new()).send().await
+                }
             }
+        } else {
+            //todo make this random
+            request_builder(
+                reqwest::Client::builder()
+                    .proxy(reqwest::Proxy::all("_").unwrap())
+                    .build()
+                    .unwrap(),
+            )
+            .send()
+            .await
         }
     }
 }
 
 pub(crate) fn request_builder(client: reqwest::Client) -> RequestBuilder {
     unsafe {
-        let mut https_builder = client
-            .get(&UNSAFE_PUB_VAR.attack_url);
+        let mut https_builder = client.get(&UNSAFE_PUB_VAR.attack_url);
         for (index, header) in UNSAFE_PUB_VAR.headers.iter().enumerate() {
             let use_header = UNSAFE_PUB_VAR.headers_val.get(index);
             match use_header {
                 None => {
                     println!("Header Val Data Was Damaged, please restart the client but don't worry it is skipping this header")
                 }
-                Some(data) => {
-                    https_builder = https_builder.header(header, data)
-                }
+                Some(data) => https_builder = https_builder.header(header, data),
             }
         }
         https_builder
     }
 }
-
 
 pub(crate) fn add_start(mut val: MutexGuard<'static, SafeGlobalVar>) {
     val.thread_on += 1.0;
@@ -89,10 +94,15 @@ pub(crate) fn udp() -> UdpSocket {
     let mut error_much: u8 = 0;
     loop {
         match UdpSocket::bind("0.0.0.0:8080") {
-            Ok(data) => { return data; }
+            Ok(data) => {
+                return data;
+            }
             Err(data) => {
                 if error_much > 10 {
-                    panic!("Failed when starting udp, please check 8080 port and try again\n {}", data);
+                    panic!(
+                        "Failed when starting udp, please check 8080 port and try again\n {}",
+                        data
+                    );
                 }
                 thread::sleep(time::Duration::from_millis(20));
                 error_much += 1;
@@ -100,6 +110,3 @@ pub(crate) fn udp() -> UdpSocket {
         }
     }
 }
-
-
-
